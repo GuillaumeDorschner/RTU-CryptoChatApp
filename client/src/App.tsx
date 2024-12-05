22285b46b8cd0183115099ba749afd77916d38f6
Guillaume Dorschner <guillaume.dorschner@icloud.com>
2024-12-05 22:39:38 +0200

    init configuration project
    - Initialized client (React + TypeScript + Vite) with TailwindCSS
    - Set up server (Node.js + TypeScript) using Express and WebSocket (ws)
    - Added shared for reusable TypeScript utilities
    - Configured concurrently for running client and server
    - Established modular TypeScript configs for all packages

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-800 text-white text-center p-4">
+      <h1 className="text-2xl font-bold">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
