9e2e8d0892596e1bdf575a4f4a700a7c81d43615
NovaWasTaKenn <98171103+NovaWasTaKenn@users.noreply.github.com>
2024-12-12 23:48:41 +0200

    Fixed

--- client/src/CryptoAlgs/Utils/WordArray.ts
+++ client/src/CryptoAlgs/Utils/WordArray.ts
@@ -1,3 +1,5 @@
+import { NumberArrayToBinary } from "./printFormat";
+
 export interface WordArray {
     words: number[];
     nbBytes: number;
@@ -26,26 +28,66 @@ export class WordArray implements WordArray {
         return clampedLeft.concatRecurse(wordArray)
     }
 
-    private concatRecurse(wordArray: WordArray, index: number=0): WordArray {
+    //private concatRecurse(wordArray: WordArray, index: number=0): WordArray {
+//
+    //    if(index>wordArray.nbBytes) return this
+    //    
+    //    if(this.nbBytes %4){
+//
+    //        const thatByte = (wordArray.words[index >>> 2] >>> (24 - (index % 4) * 8)) & 0xff;
+    //        return (new WordArray(
+    //            this.words.slice(0, Math.floor(this.nbBytes/4))
+    //                .concat(this.words[(this.nbBytes + index) >>> 2] | thatByte << (24 - ((this.nbBytes + index) % 4) * 8)),
+    //            this.nbBytes+1
+    //        )).concatRecurse(wordArray, index+1)
+    //        
+    //    }
+    //    else{
+    //        return (new WordArray(
+    //            this.words.slice(0, Math.floor(this.nbBytes/4))
+    //                .concat(wordArray.words[index>>2]),
+    //            this.nbBytes+1
+    //        )).concatRecurse(wordArray, index+4)
+//
+//
+    //    }
+//
+    //}
 
-        if(index>wordArray.nbBytes) return this
+    private concatRecurse(wordArray: WordArray): WordArray {
+
+        if(wordArray.nbBytes<=0) return this
         
+        console.log("right:"+NumberArrayToBinary(this.words))
+        console.log("left:"+NumberArrayToBinary(wordArray.words))
+
         if(this.nbBytes %4){
 
-            const thatByte = (wordArray.words[index] >>> (24 - (index % 4) * 8)) & 0xff;
-            return (new WordArray(
-                this.words.slice(0, Math.floor(this.nbBytes/4))
-                    .concat(this.words[(this.nbBytes + index) >>> 2] | thatByte << (24 - ((this.nbBytes + index) % 4) * 8)),
-                this.nbBytes+1
-            )).concat(wordArray, index+1)
+            const thatByte = (wordArray.words[0] >>> 24) & 0xff;
+            const newRightWordArray = new WordArray(
+                [...Array(wordArray.words.length).keys()].map(i=>{
+                    return wordArray.words[i]<<8 | wordArray.words[i+1]>>>24 & 0xff 
+                }),
+                wordArray.nbBytes-1
+            )
+
+            return (
+                new WordArray(
+                    this.words.slice(0, Math.floor(this.nbBytes/4))
+                        .concat(this.words[this.nbBytes >>> 2] | thatByte << (24 - (this.nbBytes % 4) * 8)),
+                    this.nbBytes+1
+                )
+            ).concatRecurse(newRightWordArray)
             
         }
         else{
+            const currentWord = wordArray.words[0]
+            const newRightWords = new WordArray(wordArray.words.slice(1), wordArray.nbBytes-4) 
             return (new WordArray(
                 this.words.slice(0, Math.floor(this.nbBytes/4))
-                    .concat(wordArray.words[index>>2]),
-                this.nbBytes+1
-            )).concat(wordArray, index+4)
+                    .concat(currentWord),
+                this.nbBytes+4
+            )).concatRecurse(newRightWords)
 
 
         }
@@ -53,18 +95,15 @@ export class WordArray implements WordArray {
     }
     
     clamp(): WordArray {
-        // Clamp
-        return new WordArray(
-            new Array(Math.ceil(this.nbBytes / 4), 
-                ...(
-                    this.words.slice(0, this.nbBytes >>> 2)
-                    .concat(this.words[this.nbBytes >>> 2] & (0xffffffff << (32 - (this.nbBytes % 4) * 8)))
-                    .concat(this.words.slice((this.nbBytes >>> 2)+1))
-                )
-            ),
-            this.nbBytes
-        ) 
+
+        const prevElements = this.words.slice(0, this.nbBytes >>> 2)
+        console.log(NumberArrayToBinary(prevElements))
+        const clampedWord = this.words[this.nbBytes >>> 2] & (0xffffffff << (32 - (this.nbBytes % 4) * 8))
+        console.log(NumberArrayToBinary([clampedWord]))
+        const otherWords = this.words.slice((this.nbBytes >>> 2)+1)
+        const newWords =  prevElements.concat(clampedWord)
     
+        return new WordArray(newWords, this.nbBytes)
     }
 
     clone(): WordArray {
