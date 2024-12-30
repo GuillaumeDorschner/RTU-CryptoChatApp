437f5eb4dd7922d537ac66f55cc9f8cc96a6056c
NovaWasTaKenn <98171103+NovaWasTaKenn@users.noreply.github.com>
2024-12-30 18:47:22 +0200

    Crypto tests

--- client/tests/AES.test.ts
+++ client/tests/AES.test.ts
@@ -1,11 +1,40 @@
-// TODO: make AES the tests
+import { AESImpl } from "../src/CryptoAlgs/AES/AES";
+import { WordArray } from "../src/CryptoAlgs/Utils/WordArray";
+import { aesConstants } from "../src/CryptoAlgs/AES/AESConstants";
 
-describe('basic', () => {
-  it('Is it the same ?', () => {
-    const a = Number(11);
-    const b = Number(11);
-    const sum = a + b;
 
-    expect(sum).toBe(Number(22));
+function getOrThrowStr(input:string|undefined): string{
+  if(typeof input === 'string'){
+    return input
+  }
+  throw new Error("string was undefined")
+}
+
+function getByteLengthUtf16(input: string): number {
+  let byteLength = 0;
+  for (const char of input) {
+    byteLength += char.charCodeAt(0) > 0xffff ? 4 : 2;
+  }
+  return byteLength;
+}
+
+const message = "new Message"
+const testPasswword = "testPassword"
+const aesAlice = (new AESImpl()).init(
+  getOrThrowStr(testPasswword), 
+  getByteLengthUtf16(getOrThrowStr(testPasswword)), 
+  aesConstants) 
+const aesBob = (new AESImpl()).init(
+  getOrThrowStr(testPasswword), 
+  getByteLengthUtf16(getOrThrowStr(testPasswword)), 
+  aesConstants) 
+
+
+describe('Whole process', () => {
+  it('Is decrypted orginal message', () => {
+    const encryptedMessage = WordArray.stringifyBase64(aesAlice.encryptMessage(message.trim(), aesConstants));
+    const decryptMessage = aesBob.decryptMessage(WordArray.parseBase64(encryptedMessage), aesConstants);
+
+    expect(decryptMessage).toBe(message);
   });
 });
--- client/tests/ECDH.test.ts
+++ client/tests/ECDH.test.ts
@@ -1,33 +1,17 @@
-// TODO: make ECDH the tests
+import { ECC } from '../src/CryptoAlgs/ECC/ecc';
+import { Point } from '../src/CryptoAlgs/ECC/point';
+import { Secp256k1 } from '../src/CryptoAlgs/ECC/curve';
 
-import { ECDH } from '../src/CryptoAlgs/ECDH/ECDH';
-import { WordArray } from '../src/CryptoAlgs/Utils/WordArray';
+const eccAlice = new ECC()
+const eccBob = new ECC()
 
 describe('ECDH Key Exchange Tests', () => {
   test('Two users should derive the same shared secret', () => {
-    const A = ECDH.generateKeyPair();
-    expect(A.publicKey).toBeInstanceOf(WordArray);
-    expect(A.privateKey).toBeInstanceOf(WordArray);
-    expect(A.publicKey.nbBytes).toBe(32);
-    expect(A.privateKey.nbBytes).toBe(32);
 
-    const B = ECDH.generateKeyPair();
-    expect(B.publicKey).toBeInstanceOf(WordArray);
-    expect(B.privateKey).toBeInstanceOf(WordArray);
-    expect(B.publicKey.nbBytes).toBe(32);
-    expect(B.privateKey.nbBytes).toBe(32);
+    const sharedKeyBob = Point.publicKeyToPoint(eccAlice.getPublicKey(), new Secp256k1()).scalarMul(BigInt(eccBob.sk)).x.toString()
+    const sharedKeyAlice = Point.publicKeyToPoint(eccBob.getPublicKey(), new Secp256k1()).scalarMul(BigInt(eccAlice.sk)).x.toString()
+    
 
-    const sharedSecretA = ECDH.computeSharedSecret(A.privateKey, B.publicKey);
-    expect(sharedSecretA).toBeInstanceOf(WordArray);
-    expect(sharedSecretA.nbBytes).toBe(32);
-
-    const sharedSecretB = ECDH.computeSharedSecret(B.privateKey, A.publicKey);
-    expect(sharedSecretB).toBeInstanceOf(WordArray);
-    expect(sharedSecretB.nbBytes).toBe(32);
-
-    console.log(sharedSecretA);
-    console.log(sharedSecretB);
-
-    expect(sharedSecretA).toEqual(sharedSecretB);
+    expect(sharedKeyBob).toEqual(sharedKeyAlice);
   });
 });
